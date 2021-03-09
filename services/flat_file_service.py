af4fba1a0f21d9192cf4e5e7ef4cc82122ce792c
Tibor Balaz Pc <tib.balaz@gmail.com>
2021-03-09 04:28:23 +0100

    Added first line file remover
    This is necessary for removal of metadata from Word2Vec file

--- services/flat_file_service.py
+++ services/flat_file_service.py
@@ -6,3 +6,13 @@ class FlatFileCreationService:
         wv = KeyedVectors.load_word2vec_format(
             location, binary=True, limit=1000000)
         wv.save_word2vec_format(target_vector_file_name)
+        # first line contains metadata
+        self.remove_first_line_from_file(target_vector_file_name)
+
+    def remove_first_line_from_file(self, file_name):
+        with open(file_name, 'r+') as f:
+            f.readline()
+            data = f.read()
+            f.seek(0)
+            f.write(data)
+            f.truncate()
