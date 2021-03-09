4903811103ea4e1661e5ea48dd5e6b5de52b6f03
Tibor Balaz Pc <tib.balaz@gmail.com>
2021-03-09 03:16:18 +0100

    Added word embedding service
    Service adding numeric vector information to existing sentences

--- services/word_embedding_service.py
+++ services/word_embedding_service.py
@@ -1,40 +1,95 @@
-from pyspark.sql import SparkSession
-from pyspark.ml.feature import Tokenizer, RegexTokenizer
-from pyspark.sql.functions import col, udf
-from pyspark.sql.types import IntegerType
+from pyspark.ml.feature import RegexTokenizer
 from pyspark.ml.feature import StopWordsRemover
+from pyspark.ml.stat import Summarizer
+from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.functions import col, explode, row_number, levenshtein, count, min
+from pyspark.sql.window import *
 
 
-class WordEmbedder:
+class WordEmbeddingService:
     def __init__(self, spark_session: SparkSession):
         super().__init__()
         self.spark_session = spark_session
+        self.sentence_col_name = 'sentence'
+        self.sentence_col_id = 'sentence_id'
+        self.word_col_name = 'word'
 
     def read_csv_without_delimitation(self, path_to_csv):
         return self.spark_session.read.format('csv').option("delimiter", "//").option(
             "header", "false").load(path_to_csv)
 
-    def tokenize(self, df: DataFrame):
+    def tokenize(self, df: DataFrame, col_to_tokenize: str):
         regex_tokenizer = RegexTokenizer(
-            inputCol='_c0', outputCol='word', pattern='\\W')
+            inputCol=col_to_tokenize, outputCol='word', pattern='\\W')
         return regex_tokenizer.transform(df)
 
     def remove_stop_words(self, df: DataFrame):
         remover = StopWordsRemover(inputCol='word', outputCol='filtered')
-        count_tokens = udf(lambda word: len(word), IntegerType())
-        return remover.transform(df).withColumn(
-            'tokens', count_tokens(col('filtered')))
+        return remover.transform(df)
 
-    def embed_words(self):
-        # do not split by delimitation
-        df_source = self.read_csv_without_delimitation(
-            '/home/jovyan/work/phrases.csv')
-        df_vect = self.spark_session.read.format('csv').option("delimiter", " ").option(
-            "header", "false").load('/home/jovyan/work/vectors.csv')
+    def embed_vector_to_not_matched_words(self, df: DataFrame, df_vector_filler: DataFrame):
+        not_matched_df = df.where(
+            col('word_vector').isNull()).select(self.sentence_col_id, 'word')
 
-        tokenized_source = self.tokenize(df_source)
+        df3 = self.assign_alternative_match_word_based_on_lavenshtein(
+            not_matched_df, df_vector_filler)
+
+        return df3.alias('base').join(df_vector_filler.alias('filler'),
+                                      df3.match == col('filler' + '.' + self.word_col_name), how='left').select(
+            self.sentence_col_id, col('base' + '.' + 'word').alias('word'),
+            col('filler' + '.' + 'word_vector').alias('word_vector'))
+
+    def assign_alternative_match_word_based_on_lavenshtein(self, not_matched_df, df_vector_filler):
+        not_matched_df_x_filler = not_matched_df.crossJoin(
+            df_vector_filler.select(col(self.word_col_name).alias('match')))
+        df1_x_df2 = not_matched_df_x_filler.withColumn("levenshtein", levenshtein(
+            col('word'), col('match')))
+        return df1_x_df2.withColumn('overall_min', min(col("levenshtein")).over(
+            Window.partitionBy(self.sentence_col_id, 'word'))) \
+            .where(col('overall_min') == col('levenshtein')) \
+            .withColumn('rank', row_number().over(Window.partitionBy(self.sentence_col_id, 'word').orderBy('match'))) \
+            .where(col('rank') == 1) \
+            .drop('levenshtein', 'overall_min', 'rank')
+
+    def embed_words(self, sentence_df: DataFrame, word_vector_df: DataFrame) -> DataFrame:
+        tokenized_source = self.tokenize(sentence_df, self.sentence_col_name)
         tokenized_source_no_stop_words = self.remove_stop_words(
             tokenized_source)
-        print(tokenized_source_no_stop_words.take(20))
-        print(df_vect.select(df_vect.columns[:2]).take(5))
+
+        clean_word_no_dup_df = self.convert_tokens_to_words_no_duplicates(
+            tokenized_source_no_stop_words)
+
+        return self.match_word_with_word_vector(clean_word_no_dup_df, word_vector_df)
+
+    def match_word_with_word_vector(self, clean_word_no_dup_df, word_vector_df):
+        words_with_vector_df = self.assign_vector_to_words(
+            clean_word_no_dup_df, word_vector_df)
+
+        mismatched_words_matched_df = self.embed_vector_to_not_matched_words(
+            words_with_vector_df, word_vector_df)
+
+        complete_match_df = words_with_vector_df.where(
+            col('word_vector').isNotNull()).union(mismatched_words_matched_df)
+
+        return complete_match_df.groupBy('sentence_id').agg(
+            Summarizer.sum(col('word_vector')).alias('sentence_vector')).select('sentence_id', 'sentence_vector')
+
+    def assign_vector_to_words(self, clean_word_no_dup_df, word_vector_df):
+        return clean_word_no_dup_df.alias('src').join(
+            word_vector_df.alias('vect'), col(
+                'src' + '.' + 'word') == col('vect' + '.' + self.word_col_name),
+            how='left') \
+            .select(self.sentence_col_id, col('src' + '.' + 'word').alias('word'), 'word_vector')
+
+    def convert_tokens_to_words_no_duplicates(self, tokenized_source_no_stop_words):
+        clean_word_df = tokenized_source_no_stop_words \
+            .drop('word') \
+            .withColumn("word", explode('filtered')).alias("word") \
+            .select(self.sentence_col_id, "word")
+        # removing duplicates
+        clean_word_no_dup_df = clean_word_df \
+            .groupBy(self.sentence_col_id, "word") \
+            .agg(count('word').alias('cnt')) \
+            .select(self.sentence_col_id, "word")
+        return clean_word_no_dup_df
