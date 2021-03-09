d2c1ac6b89841c1e8745a2f78b330fe4aab2422f
Tibor Balaz Pc <tib.balaz@gmail.com>
2021-03-09 04:20:29 +0100

    Implemented argparse for command line arguments

--- entry.py
+++ entry.py
@@ -1,5 +1,11 @@
 from manager import AssignmentManager
+import argparse
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Galytix assignment.')
+    parser.add_argument('-s', type=str, help='Sentence to get distance to closest phrase', required=False)
+
+    args = parser.parse_args()
+
     manager = AssignmentManager()
-    manager.manage('Ahoooj ako sa mas')
+    manager.manage(args.s)
